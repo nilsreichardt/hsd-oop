9dd99111d10b6c39be2eaae7d1d376ed5a6f506a
nilsreichardt <nils@sharezone.net>
2021-04-17 16:08:59 +0200

    P03: Task 2

--- OOP2/P03/src/DoLiString.java
+++ OOP2/P03/src/DoLiString.java
@@ -7,16 +7,53 @@ public class DoLiString {
         last = null;
     }
 
+    public static void main(String[] args) {
+        DoLiString list = new DoLiString();
+        System.out.println(list);
+    }
+
+    private String generateToStringWithAllNodes() {
+        Node currentNode = first;
+
+        StringBuilder s = new StringBuilder();
+
+        while(currentNode != null) {
+            // Tennung zwischen den Node, jedoch nicht für das erste Objekt, damit so ein String entsteht: ", Node(...)"
+            if(!s.isEmpty()) {
+                s.append(", ");
+            }
+
+            s.append(currentNode);
+            currentNode = currentNode.next;
+        }
+
+        return s.toString();
+    }
+
+    @Override
+    public String toString() {
+        return generateToStringWithAllNodes();
+    }
+
     private class Node {
         private Node next;
         private Node prev;
 
-        String item;
+        String payload;
 
         public Node(String item) {
-            this.item = item;
+            this.payload = item;
             next = null;
             prev = null;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "next=" + next +
+                    ", prev=" + prev +
+                    ", payload='" + payload + '\'' +
+                    '}';
         }
     }
 }

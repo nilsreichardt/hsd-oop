2f1fbcd279f21076531dec8cfd037dfd02f64ff8
nilsreichardt <nils@sharezone.net>
2021-05-12 14:14:47 +0200

    Task 2, 3, 4, 5 with Tests

--- OOP2/P06/src/StringTree.java
+++ OOP2/P06/src/StringTree.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class StringTree {
     /**
      * A root node is the starting node of tree.
@@ -10,13 +12,144 @@ public class StringTree {
         return root == null;
     }
 
-    class Node {
-        Node smaller;
-        Node bigger;
+    /**
+     * Wird nur fürs Testing gebraucht.
+     * @return Gibt die Root-Node zurück.
+     */
+    protected Node getRoot() {
+        return root;
+    }
 
-        public Node(Node smaller, Node bigger) {
-            this.smaller = smaller;
-            this.bigger = bigger;
+    public boolean contains(String searchingString) {
+        if(isEmpty()) return false;
+        Node node = new Node(searchingString);
+        return root.contains(node);
+    }
+
+    private String inorder() {
+        return root.inorder();
+    }
+
+    public void add(String addingString) {
+        Node node = new Node(addingString);
+
+        if(isEmpty()) {
+            root = node;
+        } else {
+            root.add(node);
         }
+    }
+
+    public String paint() {
+        if(isEmpty()) return "";
+        return root.paint("");
+    }
+
+    public String toString() {
+        return inorder();
+    }
+}
+
+class Node {
+    String payload;
+
+    Node smaller;
+    Node bigger;
+
+    public Node(String payload) {
+        this.payload = payload;
+    }
+
+    public String inorder() {
+        String string = "";
+
+        if(hasSmaller()) {
+            string += smaller.inorder();
+        }
+
+        string += payload + "_";
+
+        if(hasBigger()) {
+            string += bigger.inorder();
+        }
+
+        return string;
+    }
+
+    public String paint(String currentString) {
+        String string = "";
+        if(hasBigger()) {
+            string += bigger.paint(currentString + "\t");
+        }
+
+        string += currentString + payload + "\n";
+
+        if(hasSmaller()) {
+            string += smaller.paint(currentString + "\t");
+        }
+
+        return string;
+    }
+
+    public void add(Node addingNode) {
+        final int compareResult = payload.compareTo(addingNode.payload);
+        if(compareResult == 0) {
+            // The case if two payloads are equal will not be treated.
+            return;
+        }
+
+        if(compareResult > 0) {
+            if(hasSmaller()) {
+                smaller.add(addingNode);
+            } else {
+                smaller = addingNode;
+            }
+        } else {
+            if(hasBigger()) {
+                bigger.add(addingNode);
+            } else {
+                bigger = addingNode;
+            }
+        }
+    }
+
+    public boolean contains(Node searchingNode) {
+        final int compareResult = payload.compareTo(searchingNode.payload);
+        if(compareResult == 0) {
+            return true;
+        }
+
+        if(compareResult > 0) {
+            if(hasSmaller()) {
+                return smaller.contains(searchingNode);
+            }
+        } else {
+            if(hasBigger()) {
+                return bigger.contains(searchingNode);
+            }
+        }
+
+        return false;
+    }
+
+    private boolean hasSmaller() {
+        return smaller != null;
+    }
+
+    private boolean hasBigger() {
+        return bigger != null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return Objects.equals(payload, node.payload) && Objects.equals(smaller, node.smaller) && Objects.equals(bigger, node.bigger);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(payload, smaller, bigger);
     }
 }
--- OOP2/P06/src/StringTreeTest.java
+++ OOP2/P06/src/StringTreeTest.java
@@ -13,16 +13,68 @@ public class StringTreeTest {
         stringTree = new StringTree();
     }
 
+    private void fillTreeWithMockData(StringTree tree) {
+        tree.add("6");
+        tree.add("7");
+        tree.add("2");
+        tree.add("8");
+        tree.add("9");
+        tree.add("4");
+        tree.add("1");
+        tree.add("3");
+        tree.add("5");
+    }
+
     @Test
-    @DisplayName(".isEmpty should return true, if tree is empty")
+    @DisplayName(".isEmpty() should return true, if tree is empty")
     public void testIsEmptyIfTreeIsEmpty() {
         assertEquals(true, stringTree.isEmpty());
     }
 
     @Test
-    @DisplayName(".isEmpty should return false, if tree is not empty")
+    @DisplayName(".isEmpty() should return false, if tree is not empty")
     public void testIsEmptyIfTreeIsNotEmpty() {
+        assertEquals(true, stringTree.isEmpty());
+        stringTree.add("Test");
         assertEquals(false, stringTree.isEmpty());
     }
 
+    @Test
+    @DisplayName(".add() should set added node as root node, if tree is was empty")
+    public void testIfRootNodeIsSetWhenFirstNodeIsAdded() {
+        assertEquals(true, stringTree.isEmpty());
+        stringTree.add("Test");
+        assertEquals(new Node("Test"), stringTree.getRoot());
+    }
+
+    @Test
+    @DisplayName(".add() should insert 6, 7, 2, 8, 9, 4, 1, 3, 5 correctly")
+    public void testIfAddInsertCorrectly() {
+        fillTreeWithMockData(stringTree);
+        assertEquals("1_2_3_4_5_6_7_8_9_", stringTree.toString());
+    }
+
+    @Test
+    @DisplayName(".contains() should return false if searchingString is not in tree")
+    public void testIfContainsReturnsFalseIfSearchingStringIsNotInTree() {
+        assertEquals(false, stringTree.contains("IsNotInTree"));
+    }
+
+    @Test
+    @DisplayName(".contains() should return true if searchingString is in tree")
+    public void testIfContainsReturnsTrueIfSearchingStringIsInTree() {
+        fillTreeWithMockData(stringTree);
+        assertEquals(true, stringTree.contains("6"));
+        assertEquals(true, stringTree.contains("7"));
+        assertEquals(true, stringTree.contains("2"));
+        assertEquals(false, stringTree.contains("A"));
+        assertEquals(false, stringTree.contains("10"));
+    }
+
+    @Test
+    @DisplayName(".paint() should paint tree correctly")
+    public void testIfPaintWorksCorrectly() {
+        fillTreeWithMockData(stringTree);
+        assertEquals("\t\t\t9\n\t\t8\n\t7\n6\n\t\t\t5\n\t\t4\n\t\t\t3\n\t2\n\t\t1\n", stringTree.paint());
+    }
 }

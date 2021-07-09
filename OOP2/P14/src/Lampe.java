5c6c258b4a725379e6317e8a502c0fbbbc337c5b
Nils Reichardt <nils@sharezone.net>
2021-07-09 12:05:31 +0200

    OOP2 - P14 (#13)
    * All tasks of OOP2 P13
    
    * Setup
    
    * Task 1
    
    * Task 2 a)
    
    * Task 2 b)
    
    * Task 3 a)
    
    * Task 4 a)
    
    * Task 5 a) & b)

---
+++ OOP2/P14/src/Lampe.java
@@ -0,0 +1,56 @@
+public class Lampe implements Runnable {
+    /**
+     * Gibt an, ob die Lamp an oder aus ist.
+     */
+    boolean istAn = false;
+
+    /**
+     * Gibt die Farbe der Lampe als String an, z.B. rot
+     */
+    String farbe;
+
+    /**
+     * Die Verweil-Dauer gibt an, wie lange eine Lampe an oder aus bleibt. Diese Zeitangabe wird in Millisekunden
+     * angegeben.
+     */
+    int verweilDauer;
+
+    /**
+     * Gibt die Anzahl an, wie oft der Zustand (an/aus) im [verweilDauer] gewechselt werden soll.
+     */
+    int anzahlWechsel;
+
+    public Lampe(String farbe, int verweilDauer, int anzahlWechsel) {
+        this.farbe = farbe;
+        this.verweilDauer = verweilDauer;
+        this.anzahlWechsel = anzahlWechsel;
+    }
+
+    String state() {
+        return istAn ? farbe.toUpperCase() : farbe.toLowerCase();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Moin, moin, meine aktiven Freunde :)");
+
+        for (int i = 0; i < anzahlWechsel; i++) {
+            if(Thread.currentThread().isInterrupted()) {
+                return;
+            }
+
+            System.out.println("Thread.currentThread().isInterrupted(): " + Thread.currentThread().isInterrupted());
+
+            istAn = !istAn;
+            System.out.println("Der Zustand der Lampe (Farbe: " + farbe + ") wurde geändert. Der neue Zustand lautet: " + istAn);
+
+            try {
+                Thread.sleep(verweilDauer);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        System.out.println("Jetzt ist aber auch mal Schluss mit dem Wechseln! Davon noch die Lampe kaputt!!!");
+    }
+}
---
+++ OOP2/P14/src/Lampetest.java
@@ -0,0 +1,101 @@
+public class Lampetest {
+    public static void main(String[] args) {
+        // Task 2 a)
+        // eineLamp();
+
+        // Task 2 b)
+        // zweiLampen();
+
+        // Task 3 a)
+        // lampenWarten();
+
+        // Task 4 b)
+        // lampenUeberwachen();
+
+        // Task 5 b)
+        lampenStoppen();
+    }
+
+    static void eineLamp() {
+        Thread threadLampe = new Thread(new Lampe("rot", 500, 10));
+        threadLampe.start();
+    }
+
+    static void zweiLampen() {
+        Thread threadLampe1 = new Thread(new Lampe("rot", 500, 10));
+        Thread threadLampe2 = new Thread(new Lampe("blau", 400, 5));
+
+        threadLampe1.start();
+        threadLampe2.start();
+    }
+
+    static void lampenWarten() {
+        Thread threadLampe1 = new Thread(new Lampe("gelb", 500, 10));
+        Thread threadLampe2 = new Thread(new Lampe("lila", 400, 10));
+
+        try {
+            threadLampe1.start();
+            threadLampe1.join();
+
+            threadLampe2.start();
+            threadLampe2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void lampenUeberwachen() {
+        Lampe rot = new Lampe("rot", 500, 10);
+        Lampe blau = new Lampe("blau", 400, 10);
+        Lampe schwarz = new Lampe("schwarz", 300, 10);
+
+        Thread threadLampe1 = new Thread(rot);
+        Thread threadLampe2 = new Thread(blau);
+        Thread threadLampe3 = new Thread(schwarz);
+
+        threadLampe1.start();
+        threadLampe2.start();
+        threadLampe3.start();
+
+        while(threadLampe1.isAlive() || threadLampe2.isAlive() || threadLampe3.isAlive()) {
+            System.out.println(rot.state() + "," + blau.state() + "," + schwarz.state());
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static void lampenStoppen() {
+        Lampe rot = new Lampe("rot", 500, 10);
+        Lampe blau = new Lampe("blau", 400, 10);
+        Lampe schwarz = new Lampe("schwarz", 300, 10);
+
+        Thread threadLampe1 = new Thread(rot);
+        Thread threadLampe2 = new Thread(blau);
+        Thread threadLampe3 = new Thread(schwarz);
+
+        threadLampe1.start();
+        threadLampe2.start();
+        threadLampe3.start();
+
+        int rounds = 0;
+        while(threadLampe1.isAlive() || threadLampe2.isAlive() || threadLampe3.isAlive()) {
+            rounds++;
+            System.out.println("Round: " + rounds);
+            System.out.println(rot.state() + "," + blau.state() + "," + schwarz.state());
+            if(rounds >= 5) {
+                threadLampe1.interrupt();
+                threadLampe2.interrupt();
+                threadLampe3.interrupt();
+            }
+
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
